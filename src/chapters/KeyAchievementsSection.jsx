72dc1f62489af2c8137a49931f2f55b1ea942554
Ravin Anghan <111741078+ravin-dev33@users.noreply.github.com>
2025-12-15 17:10:19 +0530

    Updated KeyAchievementsSection.jsx

--- src/chapters/KeyAchievementsSection.jsx
+++ src/chapters/KeyAchievementsSection.jsx
@@ -210,7 +210,7 @@ export default function KeyAchievementsSection() {
           variants={containerVariants}
           initial="hidden"
           animate={inView ? 'visible' : 'hidden'}
-          className="grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-12"
+          className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-12"
         >
           {achievements.map((achievement, index) => (
             <AchievementCard
